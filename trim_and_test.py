0498786cabf9fda26e210ed84c88c57e0fc28a24
qzzhang <QizhouZhang@sjtu.edu.cn>
2019-12-27 22:45:29 +0800

    Test almost done

--- trim_and_test.py
+++ trim_and_test.py
@@ -1,21 +1,32 @@
 from vggTrimmedModel import TrimmedModel
 from CIFAR_DataLoader import CifarDataManager
 import numpy as np
-d = CifarDataManager()
+import os
+os.environ["TF_CPP_MIN_LOG_LEVEL"] = "0"
+os.environ["CUDA_VISIBLE_DEVICES"] = "0, 1"
+
+def test_cluster(centroid, test_images, test_labels):
+	d = CifarDataManager()
+	model = TrimmedModel(centroid)
+	model.assign_weight()
+	return model.test_cluster(test_images, test_labels)
+
+if __name__ == "__main__":
+	centroids = np.load("./kmeansRes/KMeans-10-centroids.npy")
+	labels = np.load("./kmeansRes/KMeans-10-labels.npy")
+	
+	d = CifarDataManager()
+	test_images, test_labels = d.test.next_batch(10)
+	
+	num_of_cluster = 2
+	res = [test_cluster(centroids[i], test_images, test_labels) for i in range(num_of_cluster)]
+
+	for img_num in range(len(test_images)):
+		pred = [res[cluster][img_num] for cluster in range(num_of_cluster)]
+		print(pred)
 
 
-model = TrimmedModel(target_class_id=np.random.randint(100, size=10),
-                     multiPruning=True)
 
-'''
-Todo List:
-    1. Modify the accuracy in trimmed network (Done)
-'''
-for _ in range(50):
-    test_images, test_labels = d.test.next_batch(200)
 
-    model.test_accuracy_pretrim(test_images, test_labels)
 
-    model.assign_weight()
-    model.test_accuracy(test_images, test_labels)
-    break
+		
--- vggTrimmedModel.py
+++ vggTrimmedModel.py
@@ -9,8 +9,26 @@ import keras
 import numpy as np
 import tensorflow as tf
 from sklearn.utils import shuffle
-
 os.environ["CUDA_VISIBLE_DEVICES"] = "1"
+os.environ["TF_CPP_MIN_LOG_LEVEL"] = "3"
+
+model_structure = [
+    {'name': 'Conv1/composite_function/gate:0', 'shape': 64},
+    {'name': 'Conv2/composite_function/gate:0', 'shape': 64},
+    {'name': 'Conv3/composite_function/gate:0', 'shape': 128},
+    {'name': 'Conv4/composite_function/gate:0', 'shape': 128},
+    {'name': 'Conv5/composite_function/gate:0', 'shape': 256},
+    {'name': 'Conv6/composite_function/gate:0', 'shape': 256},
+    {'name': 'Conv7/composite_function/gate:0', 'shape': 256},
+    {'name': 'Conv8/composite_function/gate:0', 'shape': 512},
+    {'name': 'Conv9/composite_function/gate:0', 'shape': 512},
+    {'name': 'Conv10/composite_function/gate:0', 'shape': 512},
+    {'name': 'Conv11/composite_function/gate:0', 'shape': 512},
+    {'name': 'Conv12/composite_function/gate:0', 'shape': 512},
+    {'name': 'Conv13/composite_function/gate:0', 'shape': 512},
+    {'name': 'FC14/gate:0', 'shape': 4096},
+    {'name': 'FC15/gate:0', 'shape': 4096},
+]
 
 
 class TrimmedModel():
@@ -21,260 +39,100 @@ class TrimmedModel():
         3. Test Accuracy
     '''
 
-    def __init__(self, target_class_id=[0], multiPruning=False):
-        self.prune_ratio = 0.9
+    def __init__(self, centroid):
+        self.prune_ratio = 0.99
 
         self.graph = tf.Graph()
         self.build_model(self.graph)
         print("restored the pretrained model......")
         self.restore_model(self.graph)
-
-        self.target_class_id = target_class_id  # assign the trim class id
-        self.multiPruning = multiPruning  # ready to prune for one single class or multi classes
-        # self.close_sess()
-
-    '''
-    Find mask class unit
-    '''
-
-    def mask_class_unit(self, classid):
-        self.test_counter = 0
-        theshold = 10
-        json_path = "./ClassEncoding/class" + str(classid) + ".json"
-        with open(json_path, "r") as f:
-            gatesValueDict = json.load(f)
-            for idx in range(len(gatesValueDict)):
-                layer = gatesValueDict[idx]
-                name = layer["name"]
-                vec = layer["shape"]
-                # process name
-                name = name.split('/')[0]
-                # process vec
-                for i in range(len(vec)):
-                    if vec[i] < 10:
-                        vec[i] = 0
-                    else:
-                        vec[i] = 1
-                layer["name"] = name
-                layer["shape"] = vec
-
-            return gatesValueDict
+        # centroid: an (1,12416) numpy array
+        self.centroid = centroid
 
     '''
     mask by value
     '''
+    def mask_cluster(self):
+        
+        def prune(gate_value, ratio):
+            """
+            gate_value: a 1-d list 
+            """
+            r_ratio = 1 - ratio
+            m = max(gate_value)
+            res = []
+            for v in gate_value:
+                if v < m * r_ratio:
+                    res.append(0)
+                else: res.append(v)
+            return res 
 
-    def mask_unit_by_value(self, classid):
-        formulizedDict = {}
-        json_path = "./ClassEncoding/class" + str(classid) + ".json"
-
-        allGatesValue = []
-
-        with open(json_path, "r") as f:
-            gatesValueDict = json.load(f)
-            for idx in range(len(gatesValueDict)):
-                layer = gatesValueDict[idx]
-                name = layer["name"]
-                vec = layer["shape"]
-                allGatesValue += vec
-
-        allGatesValue.sort()
-        allGatesValue = allGatesValue[:int(len(allGatesValue)*0.8)]
-
-        allGatesValue = set(allGatesValue)
-        with open(json_path, "r") as f:
-            gatesValueDict = json.load(f)
-            for idx in range(len(gatesValueDict)):
-                layer = gatesValueDict[idx]
-                name = layer["name"]
-                vec = layer["shape"]
-                # process name
-                name = name.split('/')[0]
-                # process vec
-                for i in range(len(vec)):
-                    if vec[i] in allGatesValue or vec[i] == 0:
-                        vec[i] = 0
-                    else:
-                        vec[i] = 1
-                layer["name"] = name
-                layer["shape"] = vec
-            with open("./ClassEncoding/mask1.json", "w") as tmpFile:
-                json.dump(gatesValueDict, tmpFile)
-            return gatesValueDict
-
-    '''
-    Fine mask class multi, merge multi-class JSONs
-    '''
-
-    def mask_class_multi(self):
-        theshold = 5
-        self.test_counter = 0
-        ''' init the dict with class0.json '''
-        multiClassGates = self.mask_class_unit(self.target_class_id[0])
-        for classid in self.target_class_id:
-            if (classid == self.target_class_id[0]):
-                continue
-            ''' Merge JSONs continuously '''
-            json_path = "./ClassEncoding/class" + str(classid) + ".json"
-            with open(json_path, "r") as f:
-                gatesValueDict = json.load(f)
-                for idx in range(len(gatesValueDict)):
-                    layer = gatesValueDict[idx]
-                    name = layer["name"]
-                    vec = layer["shape"]
-                    # process name
-                    name = name.split('/')[0]
-                    # process vec
-                    for i in range(len(vec)):
-                        if vec[i] < theshold:
-                            vec[i] = 0
-                        else:
-                            vec[i] = 1
-                    gatesValueDict[idx]["name"] = name
-                    gatesValueDict[idx]["shape"] = vec
-
-                ''' Now we merge gatesValueDict and multiClassGates '''
-                for idx1 in range(len(gatesValueDict)):
-                    for idx2 in range(len(multiClassGates)):
-                        if (gatesValueDict[idx1]["name"] == multiClassGates[idx2]["name"]):
-                            tomerge = gatesValueDict[idx1]["shape"]
-                            for idx3 in range(len(tomerge)):
-                                if (tomerge[idx3] == 1 and multiClassGates[idx2]["shape"][idx3] == 0):
-                                    multiClassGates[idx2]["shape"][idx3] = 1
-                                    self.test_counter += 1
-                                else:
-                                    pass
-                        else:
-                            pass
-            print("Furthermore, class ", str(classid), " activate nums of neurons: ", str(self.test_counter))
-
-        return multiClassGates
-
-    def mask_class_multi_by_value(self):
-        '''
-        Calculate sum of multi-class scalars
-        '''
-        print("RUNNING mask_class_multi_by_value.py")
-        # print("Pruning Ratio: ", self.prune_ratio)
-        multiClassGates = list()
-        for classid in self.target_class_id:
-            '''
-            Merge JSONs continuously
-            '''
-            json_path = "./ClassEncoding/class" + str(classid) + ".json"
-            with open(json_path, "r") as f:
-                gatesValueDict = json.load(f)
-                for idx in range(len(gatesValueDict)):
-                    layer = gatesValueDict[idx]
-                    name = layer["name"]
-                    vec = layer["shape"]
-                    # process name
-                    name = name.split('/')[0]
-                    gatesValueDict[idx]["name"] = name
-                    gatesValueDict[idx]["shape"] = vec
-                if not multiClassGates:
-                    '''
-                    Initialize the multiClassGates
-                    '''
-                    multiClassGates = gatesValueDict
-                else:
-                    '''
-                    Now we merge gatesValueDict and multiClassGates
-                    '''
-                    for idx1 in range(len(gatesValueDict)):
-                        for idx2 in range(len(multiClassGates)):
-                            if (gatesValueDict[idx1]["name"] == multiClassGates[idx2]["name"]):
-                                tomerge = gatesValueDict[idx1]["shape"]
-                                for idx3 in range(len(tomerge)):
-                                    multiClassGates[idx2]["shape"][idx3] += tomerge[idx3]
-                            else:
-                                pass
-        '''
-        Sort & Mask for multi-class conditions
-        '''
-        allGatesValue = []
-        for idx in range(len(multiClassGates)):
-            layer = multiClassGates[idx]
+        idx = 0
+        pruned_gates = []
+        nonzero_entry = 0
+        for layer in model_structure:
+            shape = layer["shape"]
             name = layer["name"]
-            vec = layer["shape"]
-            allGatesValue += vec
+            gate_value = self.centroid[idx: idx+shape].tolist()
+            # print(len(gate_value))
+            idx += shape
+            
+            pruned_gate_value = prune(gate_value, self.prune_ratio)
+            nonzero_entry += np.count_nonzero(np.array(pruned_gate_value))
+            pruned_gates.append({
+                "name": name, 
+                "shape": pruned_gate_value
+                })
+        print("Sparsity: {}".format(nonzero_entry/12416)) 
 
-        allGatesValue.sort()
-        allGatesValue = allGatesValue[:int(len(allGatesValue)*self.prune_ratio)]
-        allGatesValue = set(allGatesValue)
-
-        result = multiClassGates
-
-        for idx in range(len(result)):
-            layer = result[idx]
-            name = layer["name"]
-            vec = layer["shape"]
-            # process name
-            name = name.split('/')[0]
-            # process vec
-            for i in range(len(vec)):
-                if vec[i] in allGatesValue:
-                    vec[i] = 0
-                else:
-                    vec[i] = 1
-
-            layer["name"] = name
-            layer["shape"] = vec
-        return result
-
+        return pruned_gates
+    
     '''
     Assign trimmed weight to weight variables
     '''
-
     def assign_weight(self):
-        '''
-        Encapsulate unit-class pruning and multi-class pruning print("PRUNE FOR CLASS", self.target_class_id)
-        '''
         print("assign weights......")
-        maskDict = []
-        if (self.multiPruning == True and len(self.target_class_id) > 1):
-            maskDict = self.mask_class_multi_by_value()
-        else:
-            maskDict = self.mask_unit_by_value(self.target_class_id[0])
+        maskDict = self.mask_cluster()
+        # print(maskDict)
 
         for tmpLayer in maskDict:
-            if (tmpLayer["name"][0] == "C"):  # if the layer is convolutional layer
+            # if the layer is convolutional layer
+            if (tmpLayer["name"][0] == "C"):  
                 with self.graph.as_default():
-                    layerNum = tmpLayer["name"].strip("Conv")
+                    layerNum = tmpLayer["name"].split("/")[0].strip("Conv")
                     name = "Conv" + layerNum + "/composite_function/kernel:0"
                     for var in tf.global_variables():
+
                         if var.name == name:
                             tmpWeights = self.sess.run(var)
                             tmpMask = np.array(tmpLayer["shape"])
-
-                            tmpWeights[:, :, :, tmpMask == 0] = 0
+                            tmpWeights[:,:,:, tmpMask == 0] = 0
                             assign = tf.assign(var, tmpWeights)
+                            
                             self.sess.run(assign)
 
-                            # print(self.sess.run(self.graph.get_tensor_by_name(name))==0)
-            if (tmpLayer["name"][0] == "F"):  # if the layer is fully connected
+            # if the layer is fully connected
+            if (tmpLayer["name"][0] == "F"):  
                 with self.graph.as_default():
-                    layerNum = tmpLayer["name"].strip("FC")
+                    layerNum = tmpLayer["name"].split("/")[0].strip("FC")
                     name_W = "FC" + layerNum + "/W:0"
                     name_bias = "FC" + layerNum + "/bias:0"
                     for var in tf.global_variables():
                         if var.name == name_W:
                             tmpWeights = self.sess.run(var)
                             tmpMask = np.array(tmpLayer["shape"])
-
                             tmpWeights[:, tmpMask == 0] = 0
                             assign = tf.assign(var, tmpWeights)
                             self.sess.run(assign)
 
-                            # print(self.sess.run(self.graph.get_tensor_by_name(name_W))==0)
                         if var.name == name_bias:
                             tmpBias = self.sess.run(var)
                             tmpMask = np.array(tmpLayer["shape"])
-
                             tmpBias[tmpMask == 0] = 0
                             assign = tf.assign(var, tmpBias)
                             self.sess.run(assign)
-                            # print(self.sess.run(self.graph.get_tensor_by_name(name_bias))==0)
+                            
+        
         print("assign finished!")
         '''
         Save the model
@@ -287,45 +145,49 @@ class TrimmedModel():
     Test Accuracy
     '''
 
-    def test_accuracy_pretrim(self, test_images, test_labels):
+    # def test_accuracy_pretrim(self, test_images, test_labels):
 
+    #     start = time.time()
+    #     accuracy = self.sess.run(
+    #             self.accuracy, 
+    #             feed_dict={
+    #             self.xs: test_images,
+    #             self.ys_true: test_labels,
+    #             self.lr: 0.1,
+    #             self.is_training: False,
+    #             self.keep_prob: 1.0
+    #         })
+
+    #     end = time.time()
+    #     print("Pretrimmed Model Test Time: " + str(end - start))
+
+    #     print(accuracy)
+
+    def test_cluster(self, test_images, test_labels):
+        """
+        return prediction in ((label, credit),(label, credit),....)
+        """
         start = time.time()
-        accuracy = self.sess.run(
-            self.accuracy, feed_dict={
-                self.xs: test_images,
-                self.ys_true: test_labels,
-                self.lr: 0.1,
-                self.is_training: False,
-                self.keep_prob: 1.0
-            })
-        end = time.time()
-        print("Pretrimmed Model Test Time: " + str(end - start))
-
-        print(accuracy)
-
-    def test_accuracy(self, test_images, test_labels):
-        start = time.time()
-        ys_pred_argmax, ys_true_argmax = self.sess.run(
-            [self.ys_pred_argmax, self.ys_true_argmax], feed_dict={
-                self.xs: test_images,
-                self.ys_true: test_labels,
-                self.lr: 0.1,
-                self.is_training: False,
-                self.keep_prob: 1.0
-            })
+        ys_pred = self.sess.run(
+                self.ys_pred, 
+                    feed_dict={
+                    self.xs: test_images,
+                    self.ys_true: test_labels,
+                    self.lr: 0,
+                    self.is_training: False,
+                    self.keep_prob: 1.0}
+                )
         end = time.time()
         print("Trimmed Model Test Time: " + str(end - start))
 
-        count = 0
-        for i in range(len(ys_pred_argmax)):
-            if ys_true_argmax[i] in self.target_class_id:
-                count += 1 if ys_pred_argmax[i] == ys_true_argmax[i] else 0
-            else:
-                count += 1 if ys_pred_argmax[i] not in self.target_class_id else 0
-
-        test_accuracy = count/len(test_labels)
-        print(test_accuracy)
-
+        ys_pred = np.array(ys_pred)
+        res = []
+        for pred in ys_pred:
+            label = np.argmax(pred)
+            credit = pred[label]
+            res.append((label, credit))
+        
+        return tuple(res)
     '''
     Build VGG Network without Control Gate Lambdas
     '''
@@ -397,8 +259,9 @@ class TrimmedModel():
             '''
             Accuracy & Top-5 Accuracy
             '''
-            self.ys_pred_argmax = tf.argmax(ys_pred, 1)
-            self.ys_true_argmax = tf.argmax(self.ys_true, 1)
+            self.ys_pred = ys_pred 
+            # self.ys_pred_argmax = tf.argmax(ys_pred, 1)
+            # self.ys_true_argmax = tf.argmax(self.ys_true, 1)
             correct_prediction = tf.equal(tf.argmax(ys_pred, 1), tf.argmax(self.ys_true, 1))
             self.accuracy = tf.reduce_mean(tf.cast(correct_prediction, "float"))
 
